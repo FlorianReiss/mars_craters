0288f33819ba100bc041fff96437cbafcc4dc56d
Florian Reiss <freiss@cern.ch>
2018-10-30 14:51:47 +0100

    first try to use python wrapper

--- problem.py
+++ problem.py
@@ -4,12 +4,36 @@ import numpy as np
 import pandas as pd
 
 import rampwf as rw
+import json
+
+
+
+class dummy_score():
+    is_lower_the_better = False
+    minimum = 0.0
+    maximum = 1.0
+
+    def __init__(self, name='dummy score', precision=2):
+        self.name = name
+        self.precision = precision
+
+    def __call__(self, y_true_label_index, y_pred_label_index):
+        score = 0.5
+        return score
+    def score_function(self, ground_truths, predictions, valid_indexes=[0]):
+        print ("ground truths")
+        print(ground_truths)
+        print("predictions")
+        print(predictions)
+        return self(ground_truths, predictions)
+
 
 
 problem_title = 'Mars craters detection and classification'
 # A type (class) which will be used to create wrapper objects for y_pred
 Predictions = rw.prediction_types.make_detection()
 # An object implementing the workflow
+print ('this works?')
 workflow = rw.workflows.ObjectDetector()
 
 # The overlap between adjacent patches is 56 pixels
@@ -19,23 +43,8 @@ workflow = rw.workflows.ObjectDetector()
 minipatch = [28, 196, 28, 196]
 
 score_types = [
-    rw.score_types.DetectionAveragePrecision(name='ap'),
-    rw.score_types.OSPA(minipatch=minipatch),
-    rw.score_types.SCP(shape=(224, 224), minipatch=minipatch),
-    rw.score_types.DetectionPrecision(
-        name='prec(0)', iou_threshold=0.0, minipatch=minipatch),
-    rw.score_types.DetectionPrecision(
-        name='prec(0.5)', iou_threshold=0.5, minipatch=minipatch),
-    rw.score_types.DetectionPrecision(
-        name='prec(0.9)', iou_threshold=0.9, minipatch=minipatch),
-    rw.score_types.DetectionRecall(
-        name='rec(0)', iou_threshold=0.0, minipatch=minipatch),
-    rw.score_types.DetectionRecall(
-        name='rec(0.5)', iou_threshold=0.5, minipatch=minipatch),
-    rw.score_types.DetectionRecall(
-        name='rec(0.9)', iou_threshold=0.9, minipatch=minipatch),
-    rw.score_types.MADCenter(name='madc', minipatch=minipatch),
-    rw.score_types.MADRadius(name='madr', minipatch=minipatch)
+   
+    dummy_score()
 ]
 
 
@@ -48,12 +57,51 @@ def get_cv(X, y):
     n1 = n_tot // 3
     n2 = n1 * 2
 
-    return [(np.r_[0:n2], np.r_[n2:n_tot]),
+    return [
             (np.r_[n1:n_tot], np.r_[0:n1]),
             (np.r_[0:n1, n2:n_tot], np.r_[n1:n2])]
 
 
-def _read_data(path, typ):
+class MCVertex:
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
+  def __repr__(self):
+      #return '{0}, {1}, {2}'.format(self.x, self.y, self.z)
+      return 'MCVertex'
+  def __str__(self):
+        return 'MCVertex'
+
+
+class VeloState:
+  def __init__(self, x, y, z, tx, ty, pq): 
+    self.x = x
+    self.y = y
+    self.z = z
+    self.tx = tx
+    self.ty = ty
+  def __repr__(self):
+      return 'VeloState'
+  def __str__(self):
+        return 'VeloState'
+
+
+
+class VeloState_Cov:
+  def __init__(self, cov_x, cov_y, cov_tx, cov_ty, cov_xtx):
+    self.cov_x = cov_x
+    self.cov_y = cov_y
+    self.cov_tx = cov_tx
+    self.cov_ty = cov_ty
+    self.cov_xtx = cov_xtx
+  def __repr__(self):
+      return 'Cov'
+  def __str__(self):
+        return 'Cov'
+
+
+def _read_data(path, bla='bla'):
     """
     Read and process data and labels.
 
@@ -67,54 +115,55 @@ def _read_data(path, typ):
     X, y data
 
     """
-    test = os.getenv('RAMP_TEST_MODE', 0)
+    #loop over all json files:
 
-    suffix = ''
+    list_y = []
+    list_x = []
+    #default path is .
+    #have to set it for reading
+    path = path + '/data/'
+    for file in os.listdir(path):
+      if not file.endswith('.json'): continue
+      file_path = path + file
+      jdata = json.load(open(file_path))
+      MCVertices  = jdata['MCVertices']
+      #mc_pvs = np.array([ np.array(h['Pos'] + [h['products']] ) for key,h in MCVertices.items() ])
+      #mc_pvs = [ tuple(h['Pos'] + [h['products']] ) for key,h in MCVertices.items() ]
+      mc_pvs = [ MCVertex(*h['Pos'] ) for key,h in MCVertices.items() ]
+      list_y = list_y + [mc_pvs]
 
-    try:
-        data_path = os.path.join(path, 'data',
-                                 'data_{0}{1}.npy'.format(typ, suffix))
-        src = np.load(data_path, mmap_mode='r')
+      VeloTracks  = jdata['VeloTracks']
+      #velo_states = [tuple(h['ClosestToBeam']) for key,h in VeloTracks.items() ]
+      velo_states = [VeloState(*h['ClosestToBeam']) for key,h in VeloTracks.items() ]
+      velo_states_cov = [VeloState_Cov(*h['errCTBState']) for key,h in VeloTracks.items() ]
 
-        labels_path = os.path.join(path, 'data',
-                                   'labels_{0}{1}.csv'.format(typ, suffix))
-        labels = pd.read_csv(labels_path)
-    except IOError:
-        raise IOError("'data/data_{0}.npy' and 'data/labels_{0}.csv' are not "
-                      "found. Ensure you ran 'python download_data.py' to "
-                      "obtain the train/test data".format(typ))
+      #check if this indeed puts correct velo state and cov matrix together
+      zipped_x = [i for i in zip(velo_states, velo_states_cov)]
 
-    # convert the dataframe with crater positions to list of
-    # list of (x, y, radius) tuples (list of arrays of shape (n, 3) with n
-    # true craters on an image
+      list_x = list_x + [zipped_x]
 
-    # below code only works correctly if 'i' is sorted
-    assert labels['i'].is_monotonic_increasing
+    y_array = np.empty(len(list_y), dtype=object)
+    y_array[:] = list_y
 
-    # determine locations of craters for each patch in the labels array
-    n_true_patches = labels.groupby('i').size().reindex(
-        range(src.shape[0]), fill_value=0).values
-    # make cumulative sum to obtain start/stop to slice the labels
-    n_cum = np.array(n_true_patches).cumsum()
-    n_cum = np.insert(n_cum, 0, 0)
-
-    labels_array = labels[['row_p', 'col_p', 'radius_p']].values
-    y = [[tuple(x) for x in labels_array[i:j]]
-         for i, j in zip(n_cum[:-1], n_cum[1:])]
-    # convert list to object array of lists
-    y_array = np.empty(len(y), dtype=object)
-    y_array[:] = y
-
-    if test:
-        # return src, y
-        return src[:30], y_array[:30]
-    else:
-        return src, y_array
+    x_array = np.empty(len(list_x), dtype=object)
+    x_array[:] = list_x
+    x_array = np.array(x_array)
+    print("print x array")
+    print(x_array)
+    print((type(x_array)))
+    print((type(x_array[0])))
+    return x_array, y_array
+    #return np.array([[(1,2)],[(1,2)]]),np.array([[(2,3)],[(1,2)]])
 
 
-def get_test_data(path='.'):
+def get_test_data(path='/home/freiss/lxplus_work/public/recept/RAPID-data3/'):
     return _read_data(path, 'test')
+    #return np.array([1]),np.array([2])
 
 
-def get_train_data(path='.'):
+def get_train_data(path='/home/freiss/lxplus_work/public/recept/RAPID-data3/'):
     return _read_data(path, 'train')
+
+
+
+
