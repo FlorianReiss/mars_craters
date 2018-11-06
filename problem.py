36e741a9a70d4899d555cc247291aa1fac14046e
Florian Reiss <freiss@cern.ch>
2018-11-06 16:14:01 +0100

    move imported fiels to problem.py

--- problem.py
+++ problem.py
@@ -8,9 +8,7 @@ import json
 from rampwf.prediction_types.base import BasePrediction
 from rampwf.score_types import BaseScoreType 
 
-# our custom implementations for predictions and scoring
-from PVPredictions import PVPredictions
-from PVScore import PVScore, PVScore_total
+
 
 # ramp-kit for the RAPID challenge
 # test and train data should be in data/test and data/train
@@ -21,6 +19,246 @@ from PVScore import PVScore, PVScore_total
 
 #to do: implement various scoring algorithms
 #to do: decide on crossfolds, prediction
+class PVScore(BaseScoreType):
+    is_lower_the_better = False
+    minimum = 0.0
+    maximum = 1.0
+
+    def __init__(self, name='PV rec eff', precision=2):
+        self.name = name
+        self.precision = precision
+
+    def __call__(self, y_true_label_index, y_pred_label_index):
+        #we can us the python PVChecker -> need to transform data for it
+
+        
+        checker = PVChecker()
+        checker.load_from_ramp(y_true_label_index, y_pred_label_index)
+        
+
+        #checker = PVChecker
+        checker.calculate_eff()
+        
+        return checker.reconstructible_efficiency
+
+    def check_y_pred_dimensions(self, y_true, y_pred):
+      if len(y_true) != len(y_pred):
+        raise ValueError('Wrong y_pred dimensions: y_pred should have {} instances, ''instead it has {} instances'.format(len(y_true), len(y_pred)))
+
+
+class PVScore_total(BaseScoreType):
+    is_lower_the_better = False
+    minimum = 0.0
+    maximum = 1.0
+
+    def __init__(self, mode, name='total score', precision=2):
+        self.name = name
+        self.precision = precision
+        self.mode = mode
+
+
+    def __call__(self, y_true_label_index, y_pred_label_index):
+        #we can us the python PVChecker -> need to transform data for it
+
+        
+        checker = PVChecker()
+        checker.load_from_ramp(y_true_label_index, y_pred_label_index)
+        
+
+        #checker = PVChecker
+        checker.calculate_eff()
+        checker.final_score()
+        if self.mode == "total":
+          return checker.fin_score
+        if self.mode == "eff":
+          return checker.reconstructible_efficiency
+        if self.mode == "fake":
+          return checker.total_fake_rate
+
+    def check_y_pred_dimensions(self, y_true, y_pred):
+      if len(y_true) != len(y_pred):
+        raise ValueError('Wrong y_pred dimensions: y_pred should have {} instances, ''instead it has {} instances'.format(len(y_true), len(y_pred)))
+
+
+
+
+
+
+
+def z_dist_matched(rec_pv_z, mc_pv_z, m_distance):
+  return abs(rec_pv_z - mc_pv_z) < m_distance
+
+#define matching criteria, based on distance:
+def is_matched(rec_pv_x=0., rec_pv_y=0., rec_pv_z=0., mc_pv_x=0., mc_pv_y=0., mc_pv_z=0., m_distance=0.1):
+  return z_dist_matched(rec_pv_z, mc_pv_z, m_distance)
+#define configuration
+m_distance = 0.5
+
+#class to do checking and plots
+class PVChecker:
+  def __init__(self):
+    
+    #configuration for matching
+    self.m_mintracks = 10
+    self.m_distance = 0.3
+
+    #data frames to collect all rec and true pvs from all events
+    self.df_all_events_true_rec_pvs = pd.DataFrame()
+    self.df_all_events_fake_rec_pvs = pd.DataFrame()
+    self.df_all_events_mc_pvs = pd.DataFrame()
+
+
+  #load data
+  #here we expect arr_rec_pvs to be numpy array of array[x,y,z] and arr_mc_pvs to be numpy array of array[x,y,z, nTracks]
+  def load_data(self, arr_rec_pvs, arr_mc_pvs):
+    self.df_rec_pvs = pd.DataFrame(arr_rec_pvs)
+    self.df_rec_pvs['matched'] = 0
+    #entry number of matched MC PV, -99 if not matched
+    self.df_rec_pvs['matched_pv_key'] = -99
+    self.df_rec_pvs.columns=['x', 'y', 'z','matched', 'matched_pv_key']
+    self.df_mc_pvs = pd.DataFrame(arr_mc_pvs)
+    self.df_mc_pvs.columns=['x', 'y', 'z','nVeloTracks']
+
+
+  def load_from_ramp (self, y_true_label_index, y_pred_label_index):
+
+        #for MC_PVs in y_true_label_index:
+        #loop over event
+        for i_event in range(0, len(y_true_label_index)):
+          MCPV_arr_tot = []
+          RecPV_arr_tot = []
+          #set-up MC PVs
+          for MC_PV in y_true_label_index[i_event]:
+            MCPV_arr = np.array([MC_PV.x, MC_PV.y, MC_PV.z, MC_PV.numberTracks])
+            MCPV_arr_tot = MCPV_arr_tot + [MCPV_arr]
+          #set-up reconstructed PVs
+          for Rec_PV in y_pred_label_index[i_event]:
+            RecPV_arr = np.array(Rec_PV)
+            RecPV_arr_tot =  RecPV_arr_tot + [RecPV_arr]
+          MCPV_arr_tot = np.array(MCPV_arr_tot)
+          RecPV_arr_tot = np.array(RecPV_arr_tot)
+          self.load_data(RecPV_arr_tot, MCPV_arr_tot)
+          self.check_event_df()
+  
+
+    #check event with previously loaded data frames
+  def check_event_df(self):
+    #loop over MC PVs and find rec PV with minimum z distance
+    for mc_index, mc_pv in self.df_mc_pvs.iterrows():
+      #if mc_pv['nVeloTracks'] < self.m_mintracks: continue
+      
+      #loop over rec PVs
+      true_z = mc_pv['z']
+      min_dist = 10000.
+      index_min_dist = -99
+      matched_pv_key = -99
+      for rec_index, rec_pv in self.df_rec_pvs.iterrows():
+        rec_z = rec_pv['z']
+        dist_z = abs(true_z - rec_z)
+        if dist_z < min_dist:
+          min_dist = dist_z
+          index_min_dist = rec_index
+      rec_z = self.df_rec_pvs['z'][index_min_dist]
+      dist_z =  abs(true_z - rec_z)
+      #match rec and MC PVs, if the rec pv with minimum z distance to MC PV fullfills matching crtierion
+      if is_matched(rec_pv_z = rec_z, mc_pv_z=true_z, m_distance=self.m_distance) and not self.df_rec_pvs['matched'][index_min_dist]:
+        self.df_rec_pvs.loc[index_min_dist, 'matched'] = 1
+        self.df_rec_pvs.loc[index_min_dist, 'matched_pv_key'] = mc_index
+        
+
+
+    #test creating sub dataframes of real and fake rec pv
+    df = self.df_rec_pvs[self.df_rec_pvs.matched == 1]
+    self.df_fake_rec_pvs = self.df_rec_pvs[self.df_rec_pvs.matched == 0]
+
+    df_true = pd.DataFrame(columns=['true_x', 'true_y', 'true_z'], dtype=float)
+    for key,row in df.iterrows():
+      df_true.loc[key,['true_x','true_y','true_z']] = self.df_mc_pvs.loc[row['matched_pv_key'],['x','y','z']].values
+    df = pd.concat([df,df_true], axis = 1)
+    for dim in ['x', 'y', 'z']:
+      df['residual_'+dim] = df[dim] - df['true_'+dim] 
+    self.df_true_rec_pvs = df
+
+    self.df_all_events_true_rec_pvs = self.df_all_events_true_rec_pvs.append(self.df_true_rec_pvs, ignore_index=True)
+    self.df_all_events_fake_rec_pvs = self.df_all_events_fake_rec_pvs.append(self.df_fake_rec_pvs, ignore_index=True)
+    self.df_all_events_mc_pvs       = self.df_all_events_mc_pvs.append(self.df_mc_pvs, ignore_index=True)
+
+  def calculate_eff(self):
+    #use total data frames to count found/total PVs
+    counter_found_MC_PV = self.df_all_events_true_rec_pvs.index.size
+    counter_total_MC_PV = self.df_all_events_mc_pvs.index.size
+    counter_total_MC_PV_reconstructible = self.df_all_events_mc_pvs[self.df_all_events_mc_pvs.nVeloTracks > self.m_mintracks].index.size
+    #counter_total_MC_PV = self.df_all_events_mc_pvs.index.size
+    counter_fake_PV = self.df_all_events_fake_rec_pvs.index.size
+
+    self.total_efficiency = counter_found_MC_PV/counter_total_MC_PV
+    self.total_fake_rate = counter_fake_PV/(counter_found_MC_PV + counter_fake_PV)
+    self.reconstructible_efficiency = counter_found_MC_PV/counter_total_MC_PV_reconstructible
+
+  #print efficiencies and fake rate
+  def print_eff(self):
+    #use total data frames to count found/total PVs
+    counter_found_MC_PV = self.df_all_events_true_rec_pvs.index.size
+    counter_total_MC_PV = self.df_all_events_mc_pvs.index.size
+    counter_total_MC_PV_reconstructible = self.df_all_events_mc_pvs[self.df_all_events_mc_pvs.nVeloTracks > self.m_mintracks].index.size
+    #counter_total_MC_PV = self.df_all_events_mc_pvs.index.size
+    counter_fake_PV = self.df_all_events_fake_rec_pvs.index.size
+
+    self.total_efficiency = counter_found_MC_PV/counter_total_MC_PV
+    self.total_fake_rate = counter_fake_PV/(counter_found_MC_PV + counter_fake_PV)
+    self.reconstructible_efficiency = counter_found_MC_PV/counter_total_MC_PV_reconstructible
+    print ("found", counter_found_MC_PV, "of", counter_total_MC_PV, "primary vertices") 
+    print ("efficiency:", self.total_efficiency)
+    print (counter_total_MC_PV_reconstructible,"of", counter_total_MC_PV, "PVs are reconstructible (have more than", self.m_mintracks, "reconstructed Velo tracks)")
+    print ("reconstructible PV efficiency: ", self.reconstructible_efficiency)
+    print ("have", counter_fake_PV, "fake PVs")
+    print ("fake rate:", self.total_fake_rate)
+
+
+  #function to get determine total score
+  def final_score(self):
+    #critertia: efficiency, fake rate, sigma of residuals, means of residuals?
+    fin_score = self.reconstructible_efficiency + self.total_fake_rate
+    self.fin_score = fin_score / 2.
+    print("the final score is", self.fin_score, "!")
+
+
+
+
+class PVPredictions(BasePrediction):
+  def __init__(self, y_pred=None, y_true=None, n_samples=None):
+    if y_pred is not None:
+      self.y_pred = y_pred
+    elif y_true is not None:
+      self.y_pred = y_true
+    elif n_samples is not None:
+      self.y_pred = np.empty(n_samples, dtype=object)
+    else:
+     raise ValueError('Missing init argument: y_pred, y_true, or n_samples')
+
+  def __str__(self):
+    return 'y_pred = {}'.format(self.y_pred)
+
+  @classmethod
+
+  #combination at the moment dummy implementation
+  def combine(cls, predictions_list, index_list=None):
+    if index_list is None:  # we combine the full list
+      index_list = range(len(predictions_list))
+    y_comb_list = [predictions_list[i].y_pred for i in index_list]
+
+    n_preds = len(y_comb_list)
+    y_preds_combined = np.empty(n_preds, dtype=object)
+    #combined_predictions = cls(y_pred=predictions_list)
+    combined_predictions = cls(y_pred=predictions_list[0].y_pred)
+    return combined_predictions
+
+  @property
+  def valid_indexes(self):
+    return self.y_pred != np.empty(len(self.y_pred), dtype=np.object)
+    #return True
+
+
 
 
 
